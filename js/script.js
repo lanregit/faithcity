69cbae2f31be6054d6aa946d81e7c5034c164ce8
olanrewajum <muyideenkazeem610@gmail.com>
2020-04-06 22:11:16 +0100

    fisrt commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// $(function(){
+// 	$("#sub-menu").hide();
+// 	$("#sub-menu2").hide();
+
+// 	$("#fade").mouseenter(function(){
+// 		$("#sub-menu").show(100)
+// 	})
+// 	$("#fade").mouseleave(function(){
+// 		$("#sub-menu").hide(100)
+// 	})
+
+// 	$("#fade2").mouseenter(function(){
+// 		$("#sub-menu2").show(100)
+// 	})
+// 	$("#fade2").mouseleave(function(){
+// 		$("#sub-menu2").hide(100)
+// 	})
+
+// })
